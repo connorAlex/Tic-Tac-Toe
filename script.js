89868cb162a36d778dff82535eeee7add155699b
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-18 12:22:18 -0500

    add players getSymbol

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 //tic tac toe
 
 //players 
-const Players = () => {
-
+const Players = (num) => {
+    const getSymbol = () => {
+        if (num === 1){
+            return "X";
+        }
+        else{
+            return "O";
+        }
+    }
+    return {getSymbol};
 };
 
 //gameboard - do not touch DOM
@@ -32,12 +40,22 @@ const displayController = (() => {
             document.getElementById(i).innerHTML = boardArr[i];
         }
     };
-    render();
-    return {render};
+
+    const turn = (square, player) => {
+        //symbol is based on player
+
+        //make a move
+        gameboard.move(square, player.getSymbol);
+        //rerender the board
+        render();
+    }
+    
+    return {turn};
 })();
 
 
 //game - checks win state, current player. Does not touch DOM
 const game = (() => {
-
+    
+    
 })();
