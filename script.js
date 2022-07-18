7d03d62d3ddc5b24f8f72ef33aa62b900878d71b
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-18 14:41:02 -0500

    added checkWinner function

--- script.js
+++ script.js
@@ -26,7 +26,27 @@ const gameboard = (() => {
     const getBoard = () => {
         return board;
     }
-    return {move, getBoard};
+
+    const checkWinner = () => {
+        let wins = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        for (let i = 0; i < wins.length; i++){
+            let [a,b,c] = wins[i];
+            if (board[a] === board[b] && board[a] === board[c] && board[a]) {
+                return true;
+            }
+        }
+        return false;
+    }
+    return {move, getBoard, checkWinner};
 })();
 
 
@@ -72,7 +92,7 @@ const game = (() => {
             displayController.turn(element.id, playerTwo);
         }
         isPlayerOne = !isPlayerOne;
-        
+        console.log(gameboard.checkWinner());
     };
 
     return {playRound};
