5129002a5f3059f07fa4664442491a8987e80e41
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-18 11:50:07 -0500

    add render function

--- script.js
+++ script.js
@@ -8,18 +8,32 @@ const Players = () => {
 //gameboard - do not touch DOM
 const gameboard = (() => {
     let board =  Array(9).fill(null);
+
     const move = (space, symbol) => {
         let tmp  = board;
         tmp[space] = symbol;
         board = tmp;
     };
+
+    const getBoard = () => {
+        return board;
+    }
+    return {move, getBoard};
 })();
 
 
 
 //displayController - ties DOM to board
 const displayController = (() => {
+    //render function
+    const render = () => {
+        let boardArr = gameboard.getBoard();
+        for (let i = 0; i < boardArr.length; i++) {
+            document.getElementById(i).innerHTML = boardArr[i];
+        }
+    };
 
+    return {render};
 })();
 
 
