d6e168c472c15761210b1cb399060ecbf5d7cb62
connorAlex <alex.connor.cochrane@gmail.com>
2022-07-18 14:22:50 -0500

    added working x/o interaction

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //tic tac toe
 
 //players 
-const Players = (num) => {
+const Player = (num) => {
     const getSymbol = () => {
         if (num === 1){
             return "X";
@@ -9,13 +9,13 @@ const Players = (num) => {
         else{
             return "O";
         }
-    }
+    };
     return {getSymbol};
 };
 
 //gameboard - do not touch DOM
 const gameboard = (() => {
-    let board =  Array(9).fill("1");
+    let board =  Array(9).fill(null);
 
     const move = (space, symbol) => {
         let tmp  = board;
@@ -40,22 +40,40 @@ const displayController = (() => {
             document.getElementById(i).innerHTML = boardArr[i];
         }
     };
-
+    
     const turn = (square, player) => {
-        //symbol is based on player
-
-        //make a move
-        gameboard.move(square, player.getSymbol);
+        //make a move 
+        gameboard.move(square, player.getSymbol());
         //rerender the board
         render();
     }
     
+    let cells = document.querySelectorAll(".cell");
+    cells.forEach((e) => {
+        e.addEventListener("click", function() {game.playRound(this)});
+    });
     return {turn};
 })();
 
 
 //game - checks win state, current player. Does not touch DOM
 const game = (() => {
-    
-    
+    let playerOne = Player(1);
+    let playerTwo = Player(2);
+    console.log(playerOne.getSymbol());
+    let isPlayerOne = true;
+    //all together now, this wil be the function on the event listener
+    const playRound = (element) => {
+        
+        if (isPlayerOne) {
+            displayController.turn(element.id, playerOne);
+        }
+        else{
+            displayController.turn(element.id, playerTwo);
+        }
+        isPlayerOne = !isPlayerOne;
+        
+    };
+
+    return {playRound};
 })();
